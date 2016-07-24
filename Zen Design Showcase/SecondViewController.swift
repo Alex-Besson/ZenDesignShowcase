440746b3963eac42c7fda75c4ef28615d21202ba
Alex-Besson <alex_j67@hotmail.com>
2016-07-24 02:38:15 -0400

    Initial commit

---
+++ Zen Design Showcase/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Zen Design Showcase
+//
+//  Created by Alexander Besson on 2016-07-24.
+//  Copyright © 2016 Alexander Besson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Zen Design ShowcaseTests/Zen_Design_ShowcaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Zen_Design_ShowcaseTests.swift
+//  Zen Design ShowcaseTests
+//
+//  Created by Alexander Besson on 2016-07-24.
+//  Copyright © 2016 Alexander Besson. All rights reserved.
+//
+
+import XCTest
+@testable import Zen_Design_Showcase
+
+class Zen_Design_ShowcaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
